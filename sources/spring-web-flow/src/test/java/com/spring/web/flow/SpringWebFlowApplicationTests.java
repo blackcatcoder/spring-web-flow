08f81bee670d09841d290207cb9d7bec4463e013
blackcat <vinv.2491@hotmail.com>
2023-05-02 15:05:46 +0700

    init project

---
+++ sources/spring-web-flow/src/test/java/com/spring/web/flow/SpringWebFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.web.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
